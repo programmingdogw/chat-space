a614dc3ad5d89ccbd16d849c72a83f7fd8cb2718
programmingdogw <59106983+programmingdogw@users.noreply.github.com>
2020-01-14 21:54:31 +0900

    帰る前にコミット
    めっちゃコメントしてるから後で整形しないとね、後done側ではappendとテキスト空とボタン押せるのやってるし、エラーの場合の処理も書いてるから忘れないように

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,18 @@
 $(function(){
+  function buildHTML(comment){
+    var html = `
+    
+    `
+// 上のテンプレートリテラル部分に必要なHTML組み立てて置いて、後で、データ受け取ったdoneでまた組み立てて
+// appendして、テキストの部分からにして、ボタン押せるようにして返す
+// のかと思ったが、模範解答はappendしてないな、積み重なってうまくいくのか？？
+// まぁそこは後で作るのかもしれんが、後はフォームからdataで受け取ったデータをメッセージ部分にレンダリングすればいいはず
+// メインの部分とサイドの部分両方のデータが変わるかはよくわからんが、とにかくレンダリングできるようにならないと
+// 解答は恐らくメインボディのメッセージ部分しかレンダリングしてないと思う
+// まぁ続き読んでから実装した方が良さそうだ
+
+    return html;
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault()
     var formData = new FormData(this);
@@ -10,6 +24,15 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data); 
+      $('.comments').append(html);
+      $('.textbox').val('');
+      $('.form__submit').prop('disabled', false);
+    })
+    .fail(function(){
+      alert('error');
     })
   });
 });
